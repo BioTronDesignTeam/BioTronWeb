fdd0be37eddd426c1ae954f055028d263c639d7f
Morgan Liew <morganliew21@gmail.com>
2026-03-15 19:49:37 -0400

    Created MCU status component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import { BatteryStatus, Header, EmergencyStop, ParameterTuning, MotorControl} from "./components";
+import { BatteryStatus, Header, EmergencyStop, ParameterTuning, MotorControl, MCUStatus} from "./components";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <main className="bg-blue-950 h-screen p-8">
-        <BatteryStatus />
-        <EmergencyStop />
-        <ParameterTuning />
-        <MotorControl />
+      <main className="bg-sky-200 h-screen p-8">
+        <BatteryStatus/>
+        <MCUStatus/>
       </main>
     </div>
   );
