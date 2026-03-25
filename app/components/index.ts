7480daa73223a027f00f7bc540011eac8b750e0a
Morgan Liew <morganliew21@gmail.com>
2026-03-25 16:44:59 -0400

    Deleted old design

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,3 @@
 export {default as BatteryStatus} from './BatteryStatus'
 export {default as Header} from './Header'
-export {default as EmergencyStop} from './EmergencyStop'
-export {default as ParameterTuning} from './ParameterTuning'
-export {default as MotorControl} from './MotorControl'
 export {default as MCUStatus} from './MCUStatus'
